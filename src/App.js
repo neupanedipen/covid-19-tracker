5d62b16bab203e3847484cfa97ff84f187463a50
dipendra <deardipen50@gmail.com>
2020-05-26 11:40:07 +0545

    added link to blog

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
       <div className="container">
         <h1 className='title'>Covid 19 Tracker App</h1>
         <SearchCountry />
+        <strong><a href="https://neupanedipendra.com.np/simple-covid-19-tracker-app-with-react-js/" >See Full Documentation at my blog</a></strong>
       </div>
     );
   }
